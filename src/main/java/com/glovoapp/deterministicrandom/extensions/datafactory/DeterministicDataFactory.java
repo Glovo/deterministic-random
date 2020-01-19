d297df8e8071f82dbb57674d8a69d776b8b90561
Paweł Szymański <pawel.szymanski@glovoapp.com>
2020-01-19 16:20:05 +0100

    Add documentation for DeterministicDataFactory

--- src/main/java/com/glovoapp/deterministicrandom/extensions/datafactory/DeterministicDataFactory.java
+++ src/main/java/com/glovoapp/deterministicrandom/extensions/datafactory/DeterministicDataFactory.java
@@ -10,17 +10,32 @@ import org.fluttercode.datafactory.impl.DataFactory;
  * <h1 color="red"> This class is unsafe to use in production environment!</h1><br/>
  * <h2 color="red">Use this class for anything but testing at your own risk.</h2><br/>
  * Provides a global instance of {@link DataFactory} that generate values with {@link
- * DeterministicRandom}.
+ * DeterministicRandom}. Due to the limitations in extensibility of {@link DataFactory}, only one
+ * instance may exist at a time. See {@link #createDataFactory(DeterministicRandom)} for more
+ * information about this issue.
  */
 public final class DeterministicDataFactory {
 
     private static final Logger LOG = Logger.getLogger(DeterministicDataFactory.class.getName());
     private static final AtomicReference<DataFactory> DATA_FACTORY = new AtomicReference<>(null);
 
+    /**
+     * Creates a global instance of {@link DataFactory}. Only one instance may exist at a time, see
+     * {@link #createDataFactory(DeterministicRandom)} for ore information about this issue.
+     *
+     * @return a new {@link DataFactory} that generate values with {@link DeterministicRandom}
+     */
     public static DataFactory create() {
         return create(new DeterministicRandom());
     }
 
+    /**
+     * Creates a global instance of {@link DataFactory}. Only one instance may exist at a time, see
+     * {@link #createDataFactory(DeterministicRandom)} for ore information about this issue.
+     *
+     * @param deterministicRandom to be used for generating values
+     * @return a new {@link DataFactory} that generate values with given {@link DeterministicRandom}
+     */
     public static DataFactory create(final DeterministicRandom deterministicRandom) {
         return DATA_FACTORY.updateAndGet(currentFactory -> {
             if (currentFactory != null) {
@@ -31,6 +46,16 @@ public final class DeterministicDataFactory {
         });
     }
 
+    /**
+     * Creates a new instance of {@link DataFactory} that uses given {@link DeterministicRandom} for
+     * generating values. This method must be used at most once, otherwise the data may lose it's
+     * deterministic properties. This is caused by the {@link DataFactory} approach to random values
+     * generation. The {@link java.util.Random} instance is provided as a static field, therefore
+     * making it impossible to choose a different instance. As a workaround, this method injects
+     * given {@link DeterministicRandom} into that static field using reflection, effectively
+     * overwriting it. If multiple such injections were to be done during a parallel tests
+     * execution, the data would no longer be deterministic (race condition).
+     */
     private static DataFactory createDataFactory(final DeterministicRandom deterministicRandom) {
         try {
             LOG.fine("setting global random field of " + DataFactory.class.getName());
