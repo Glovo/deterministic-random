e6e146dc17ab05ddf09c09838cbd444672c0d64e
Paweł Szymański <pawelfiu@gmail.com>
2020-04-12 17:37:14 +0200

    Add JavaDoc for EffortlessFaker

--- src/main/java/com/glovoapp/effortlessfaker/EffortlessFaker.java
+++ src/main/java/com/glovoapp/effortlessfaker/EffortlessFaker.java
@@ -4,6 +4,18 @@ import javax.annotation.Nonnull;
 
 /**
  * Allows easy creation of fake objects. The {@link #fillIn(Class)} method should be the only entry-point to this API.
+ * Compatible classes must:
+ * <p><ul>
+ * <li>not inherit from any other class
+ * <li>have constructor (of any visibility) that allows creating them
+ * <li>are composed of primitive types or other data classes
+ * </ul><p>
+ * Each {@link EffortlessFaker} implementation should strive to:
+ * <p><ul>
+ * <li>require at most single line of code for initializing any data class
+ * <li>be easily configurable (achieved via {@link DataProvider} interface)
+ * <li>be as type-safe as possible
+ * </ul><p>
  *
  * @see EffortlessFakerBuilder
  */
