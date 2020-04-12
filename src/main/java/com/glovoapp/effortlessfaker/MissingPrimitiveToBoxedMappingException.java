dce255640d4f409227b3ba5be9d3d869b9027c13
Paweł Szymański <pawelfiu@gmail.com>
2020-04-12 14:57:06 +0200

    Cleanup exception modifiers

--- src/main/java/com/glovoapp/effortlessfaker/MissingPrimitiveToBoxedMappingException.java
+++ src/main/java/com/glovoapp/effortlessfaker/MissingPrimitiveToBoxedMappingException.java
@@ -1,6 +1,6 @@
 package com.glovoapp.effortlessfaker;
 
-final class MissingPrimitiveToBoxedMappingException extends RuntimeException {
+public final class MissingPrimitiveToBoxedMappingException extends RuntimeException {
 
     MissingPrimitiveToBoxedMappingException(final Class<?> primitiveType) {
         super("mapping for " + primitiveType + " is missing");
--- src/main/java/com/glovoapp/effortlessfaker/reflection/DataClassHasNoConstructorsException.java
+++ src/main/java/com/glovoapp/effortlessfaker/reflection/DataClassHasNoConstructorsException.java
@@ -1,6 +1,6 @@
 package com.glovoapp.effortlessfaker.reflection;
 
-final class DataClassHasNoConstructorsException extends RuntimeException {
+public final class DataClassHasNoConstructorsException extends RuntimeException {
 
     DataClassHasNoConstructorsException(final Class<?> dataClass) {
         super("data class " + dataClass + " declares no constructors; this should never happen");
